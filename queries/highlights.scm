ab539fd3b647475c5e2270640e2247b7891bd3fd
Ilya Ovdin <iovdin@gmail.com>
2024-12-10 15:28:55 +0100

    add roles highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,1 +1,22 @@
 (role) @variable
+
+((role) @role.user
+ (#eq? @role.user "u:"))
+
+((role) @role.assistant
+ (#eq? @role.assistant "a:"))
+
+((role) @role.system
+ (#eq? @role.assistant "s:"))
+
+((role) @role.tool_call
+ (#eq? @role.tool_call "tc:"))
+
+((role) @role.tool_result
+ (#eq? @role.tool_result "tr:"))
+
+((role) @role.comment
+ (#eq? @role.comment "c:"))
+
+((role) @role.err
+ (#eq? @role.err "err:"))
